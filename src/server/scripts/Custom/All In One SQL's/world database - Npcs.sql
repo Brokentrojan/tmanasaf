26c31154824e95cffaa5f338181169b20ed95445
Brokentrojan <Maxjunk96@gmail.com>
2016-01-16 02:59:43 +0000

    Update world database - Npcs.sql

--- src/server/scripts/Custom/All In One SQL's/world database - Npcs.sql
+++ src/server/scripts/Custom/All In One SQL's/world database - Npcs.sql
@@ -40,7 +40,7 @@ INSERT INTO `creature_template` (`entry`, `modelid1`, `modelid2`, `name`, `subna
 (@Entry, 21249, 0, @Name, @Subname, @IconName, 0, 80, 80, 2, 35, 3, 1, 0, 0, 2000, 0, 1, 0, 7, 138936390, 0, 0, 0, '', 0, 3, 1, 0, 0, 1, 0, 0, 'npc_solo3v3');
 
 SET
-@Entry = 190005,
+@Entry = 190050,
 @Name = "Beastmaster",
 @Subname = "Beast",
 @IconName = "Speak";
