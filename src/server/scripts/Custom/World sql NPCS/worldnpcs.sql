8f3f90097df15892e8093688d95d9a37410235cc
Brokentrojan <Maxjunk96@gmail.com>
2016-01-16 02:11:47 +0000

    Update worldnpcs.sql

--- src/server/scripts/Custom/World sql NPCS/worldnpcs.sql
+++ src/server/scripts/Custom/World sql NPCS/worldnpcs.sql
@@ -36,8 +36,8 @@ INSERT INTO `creature_template` (`entry`, `modelid1`, `modelid2`, `name`, `subna
 (@Entry, 21249, 0, @Name, @Subname, @IconName, 0, 80, 80, 2, 35, 3, 1, 0, 0, 2000, 0, 1, 0, 7, 138936390, 0, 0, 0, '', 0, 3, 1, 0, 0, 1, 0, 0, 'ArenaTeamRanks');
 
 SET
-@Entry = 190005,
-@Name = "Beastmaster",
+@Entry = 190050,
+@Name = "beastmaster",
 @Subname = "Beast",
 @IconName = "Speak";
 
